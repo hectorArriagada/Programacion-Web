52d2185e473c02c410b56629a5c1571c194674f3
beIturre <be.iturre@duocuc.cl>
2024-05-15 23:51:06 -0400

    Integracion de pantalla de carga en el inicio

--- js/carga.js
+++ js/carga.js
@@ -1,3 +1,5 @@
+/*
+
 window.addEventListener("DOMContentLoaded", () => {
     showLoader();
   })
@@ -15,4 +17,35 @@ window.addEventListener("DOMContentLoaded", () => {
   }
   const hideLoader = () => {
     loader.classList.remove("show_loader");
-  }
+  } 
+
+  
+   
+
+  function mostrarLoader() {
+    var loader = document.getElementById("loader");
+    loader.classList.add("show_loader");
+}
+
+function ocultarLoader() {
+    var loader = document.getElementById("loader");
+    loader.classList.remove("show_loader");
+} */
+
+function mostrarLoader() {
+  var loader = document.getElementById('loader');
+  loader.style.display = 'block'; // Muestra el loader
+
+  // Después de unos segundos (por ejemplo, 3 segundos), oculta el loader
+  setTimeout(ocultarLoader, 3000);
+}
+
+function ocultarLoader() {
+  var loader = document.getElementById('loader');
+  loader.style.display = 'none'; // Oculta el loader
+}
+
+// Muestra el loader al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+  mostrarLoader();
+});
